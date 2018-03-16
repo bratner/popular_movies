fe33b256d6e3762dea3d537dee5f2345c35e9b52
Boris Ratner <Boris.Ratner@redbend.com>
2018-03-16 12:11:42 +0200

    Movie details passed to Details activity.

--- app/src/main/java/il/co/ratners/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/il/co/ratners/popularmovies/MovieDetailsActivity.java
@@ -2,6 +2,7 @@ package il.co.ratners.popularmovies;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,9 +13,18 @@ public class MovieDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        ActionBar actionBar = getSupportActionBar();
         Intent i = getIntent();
         String originalTitle = i.getStringExtra(getString(R.string.key_original_title));
         String overview = i.getStringExtra(getString(R.string.key_overview));
+        String releaseDate = i.getStringExtra(getString(R.string.key_date));
+        String rating = i.getStringExtra(getString(R.string.key_rating));
+        String title = i.getStringExtra(getString(R.string.key_title));
+
+        actionBar.setTitle(title);
+
+
+       // String posterURL = i.getStringExtra(R.string.key_poster);
 
     }
 }
