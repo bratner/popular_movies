c92750839d153f5a8f6a2c7a19192d9653bd37f0
Boris Ratner <ratner2@gmail.com>
2018-04-14 01:01:32 +0300

    [PM2-14] Have the files. Now just to fill them

---
+++ app/src/main/java/il/co/ratners/popularmovies/data/FavoritesProvider.java
@@ -0,0 +1,50 @@
+package il.co.ratners.popularmovies.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class FavoritesProvider extends ContentProvider {
+    /*
+        So here we create the dbhelper and use it to fullfill the queries
+
+        The request comes in the form of Uri, we use uri matcher to verify the request
+        and then act accordingly. These are part of the content provider why would the
+        matcher be created anywhere else?
+     */
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
