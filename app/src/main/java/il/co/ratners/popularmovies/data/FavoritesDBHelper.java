378be4f337e0e5ecaeb830276b2a7026a0885165
Boris Ratner <ratner2@gmail.com>
2018-04-14 19:54:19 +0300

    [PM2-14] Getting closer to finishing SQL part of things

--- app/src/main/java/il/co/ratners/popularmovies/data/FavoritesDBHelper.java
+++ app/src/main/java/il/co/ratners/popularmovies/data/FavoritesDBHelper.java
@@ -3,9 +3,11 @@ package il.co.ratners.popularmovies.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class FavoritesDBHelper extends SQLiteOpenHelper {
 
+    public static String TAG = FavoritesDBHelper.class.getSimpleName();
     /* TODO: Change this to whatever we actually need */
     public FavoritesDBHelper(Context context,
                              String name,
@@ -17,11 +19,15 @@ public class FavoritesDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        final String createQuery = "";
+        db.execSQL(createQuery);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Log.d(TAG, "Deleting the Database.");
+        final String dropQuery = "DROP TABLE "+FavoritesContract.FavoritesEntry.TABLE_NAME + ";";
     }
+
 }
